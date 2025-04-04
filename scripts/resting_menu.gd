5263dadfeae148f2819727b06e09f82867dd3df0
voyager7 <kenningspath@gmail.com>
2025-04-04 01:03:30 -0400

    finished grain and blood count labels

--- scripts/resting_menu.gd
+++ scripts/resting_menu.gd
@@ -4,10 +4,14 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	# sample lifetime grain count
 	var lifetime_grain_count_label = get_node("GrainCountContainer/LifetimeGrainCount")
 	lifetime_grain_count_label.text = str(stats.lifetime_grain_count)
+	
+	# sample blood count
+	var blood_count_label = get_node("BloodCountContainer/BloodCount")
+	blood_count_label.text = str(stats.blood) + " ml."
 	#ResourceSaver.save(stats, "res://resources/test_player_stats.tres")
-	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
