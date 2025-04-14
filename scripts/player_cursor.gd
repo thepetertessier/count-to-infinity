4cae41d0a7fadfa3cee8725c1eb184843e437d51
Peter Tessier <fpv5gr@virginia.edu>
2025-04-14 14:14:48 -0400

    test out cursor improvement

--- scripts/player_cursor.gd
+++ scripts/player_cursor.gd
@@ -1,8 +1,11 @@
-extends CharacterBody2D
+extends RigidBody2D
 
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	#Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	pass
 
 func _physics_process(delta):
-	move_and_collide(get_global_mouse_position() - global_position)
+	var to_go = get_global_mouse_position() - global_position
+	#velocity = to_go / delta
+	#move_and_slide()
+	move_and_collide(to_go)
