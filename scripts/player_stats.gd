f861ebaf0b13351e18266c0ab97eb94ad181aa69
Peter Tessier <fpv5gr@virginia.edu>
2025-04-19 17:05:19 -0400

    add default stats directly resource

--- scripts/player_stats.gd
+++ scripts/player_stats.gd
@@ -3,17 +3,17 @@ class_name PlayerStats
 extends Resource
 
 # resources
-@export var lifetime_grain_count: int
-@export var blood: int
+@export var lifetime_grain_count: int = 0
+@export var blood: int = 0
 
 # stats
-@export var click_power: int
-@export var click_radius: float
-@export var auto_collect_rate: float
-@export var daylight_timer: int
+@export var click_power: int = 1
+@export var click_radius: float = 1.0
+@export var auto_collect_rate: float = 0.0
+@export var daylight_timer: int = 60
 
 # upgrade levels
-@export var long_fingers_level: int
-@export var powerful_fingers_level: int
-@export var auto_fingers_level: int
-@export var speed_demon_level: int
+@export var long_fingers_level: int = 1
+@export var powerful_fingers_level: int = 1
+@export var auto_fingers_level: int = 1
+@export var speed_demon_level: int = 1
