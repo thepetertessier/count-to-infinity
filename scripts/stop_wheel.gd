3df1aa149090ba7b91f9dcc5f4f10a9642ce780c
voyager7 <kenningspath@gmail.com>
2025-04-23 05:12:03 -0400

    added sound effects to wheel scene

--- scripts/stop_wheel.gd
+++ scripts/stop_wheel.gd
@@ -2,3 +2,9 @@ extends TextureButton
 
 func _on_pressed() -> void:
 	self.hide()
+	
+	var endless_wheel_spin = get_node("../EndlessWheelSpinSoundEffect")
+	endless_wheel_spin.stop()
+	
+	var ending_spinner = get_node("../EndingSpin")
+	ending_spinner.play()
